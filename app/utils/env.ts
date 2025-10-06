282f1f2184b5a4ba7d84db1a204f69afc14c75aa
Berkin Koray <73588639+Berkin95@users.noreply.github.com>
2025-10-06 21:48:00 +0200

    Add environment configuration and update app settings
    - Created a new `.env.example` file with API and WebSocket URLs, and Google Maps API key.
    - Updated `app.json` to include bundle identifiers, permissions, and Google Maps configuration for both iOS and Android.
    - Added new dependencies for location services and notifications in `package.json`.
    - Introduced `env.ts` utility for accessing environment variables in the app.
    - Updated `package-lock.json` to reflect new dependencies and versions.

---
+++ app/utils/env.ts
@@ -0,0 +1,6 @@
+export const Env = {
+  API_URL: process.env.EXPO_PUBLIC_API_URL!,
+  WS_URL: process.env.EXPO_PUBLIC_WS_URL!,
+  WEBHOOK_URL: process.env.EXPO_PUBLIC_WEBHOOK_URL || null,
+  MAPS_KEY: process.env.EXPO_PUBLIC_GMAPS_API_KEY!,
+};
